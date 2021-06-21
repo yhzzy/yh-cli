7f879577b628e7632c967f2ad19e7004e3c18c55
yhzzy <176145910@qq.com>
2021-06-21 17:51:48 +0800

    修改

--- bin/cli.js
+++ bin/cli.js
@@ -1,24 +1,2 @@
 #!/usr/bin/env node
-
-const path = require('path')
-const fs = require('fs')
-const inquirer = require('inquirer')
-
-const questions = [
-  {
-    type: 'input',
-    name: 'name',
-    message: 'Project name?',
-    default: __dirname,
-  },
-  {
-    type: 'confirm',
-    name: 'confirm',
-    message: 'confirm name?',
-  },
-]
-
-inquirer.prompt(questions)
-.then(answers => {
-  console.log(answers);
-})
+require('../lib/main')
